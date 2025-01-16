8dd6a30965857899f328435be7478ce693a85b82
Josh <68549085+J-716@users.noreply.github.com>
2025-01-16 10:56:12 -0500

    feat: add invoice deletion functionality; implement DeleteInvoice action and confirmation page

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -6,18 +6,27 @@ import { Button } from "./ui/button";
 
 interface SubmitButtonProps {
 	text: string;
+	variant?:
+		| "default"
+		| "destructive"
+		| "outline"
+		| "secondary"
+		| "ghost"
+		| "link"
+		| null
+		| undefined;
 }
 
-const SubmitButton = ({ text }: SubmitButtonProps) => {
+const SubmitButton = ({ text, variant }: SubmitButtonProps) => {
 	const { pending } = useFormStatus();
 	return (
 		<>
 			{pending ? (
-				<Button className="w-full" disabled>
+				<Button className="w-full" variant={variant} disabled>
 					<LoaderCircle className="mr-2 size-2 animate-spin" />
 				</Button>
 			) : (
-				<Button className="w-full" type="submit">
+				<Button className="w-full" variant={variant} type="submit">
 					{text}
 				</Button>
 			)}
