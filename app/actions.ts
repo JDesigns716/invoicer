17830efb3716eb70a04380013ddf5b7da3df876b
Josh <68549085+J-716@users.noreply.github.com>
2025-01-16 23:46:06 -0500

    feat: update invoice link generation to use correct endpoint for production

--- app/actions.ts
+++ app/actions.ts
@@ -92,8 +92,7 @@ export async function createInvoice(prevState: any, formData: FormData) {
 			invoiceLink:
 				process.env.NODE_ENV !== "production"
 					? `http://localhost:3000/api/invoice/${data.id}`
-					: // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
-						`https://invoicer-lilac.vercel.app/api/invoices`,
+					: `https://invoicer-lilac.vercel.app/api/invoice/${data.id}`,
 		},
 	});
 
@@ -161,8 +160,7 @@ export async function editInvoice(prevState: any, formData: FormData) {
 			invoiceLink:
 				process.env.NODE_ENV !== "production"
 					? `http://localhost:3000/api/invoice/${data.id}`
-					: // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
-						`https://invoicer-lilac.vercel.app/api/invoices`,
+					: `https://invoicer-lilac.vercel.app/api/invoice/${data.id}`,
 		},
 	});
 
